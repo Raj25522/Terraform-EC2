38281ff0170851f406b5775058bdbe786cfe1ea8
Satish Prabhala <122028988+Raj25522@users.noreply.github.com>
2024-03-02 20:20:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
+  access_key = ""
+  secret_key = ""
   region = "ap-south-1"
 }
